e452540d37ce53067401d2bb52b1a20b70f0d23c
kaito13380 <56272048+kaito13380@users.noreply.github.com>
2022-06-23 03:35:03 +0700

    char

---
+++ src/pages/PageTest.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+
+const PageTest = () => {
+  const [chartData, setChartData] = useState({});
+  const chart = () => {
+    let empSal = [];
+    let empAge = [];
+    axios
+      .get("http://dummy.restapiexample.com/api/v1/employees")
+      .then(res => {
+        console.log(res);
+        for (const dataObj of res.data.data) {
+          empSal.push(parseInt(dataObj.employee_salary));
+          empAge.push(parseInt(dataObj.employee_age));
+        }
+        setChartData({
+          labels: empAge,
+          datasets: [
+            {
+              label: "test char",
+              data: empSal,
+              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+              borderWidth: 4
+            }
+          ]
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log(empSal, empAge);
+  };
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div className="App">
+     
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "test char 2", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageTest;
